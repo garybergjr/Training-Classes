d5fd9402cceff399eb0751310feca093d5cf0057
garybergjr <39468694+garybergjr@users.noreply.github.com>
2018-05-28 18:15:30 -0400

    testing elseif

---
+++ root/scripts/else_if.js
@@ -0,0 +1,11 @@
+var value1 = 5;
+var value2 = 7;
+var value3 = 12;
+
+if (value1 <= value2) {
+  console.log(value1 + " is less than or equal to " + value2);
+} else if (value1 >= value2) {
+  console.log(value1 + " is greater than or equal to " + value2);
+} else if (value1 + value2 == value3) {
+  console.log (value1 + " + " + value2 + " is equal to " + value3);
+}
--- root/scripts/if_else.js
+++ root/scripts/if_else.js
@@ -1,8 +1,8 @@
 var value1 = 4;
 var value2 = 9;
 
-if (value1 < value2) {
+if (value1 <= value2) {
   console.log(value1 + " is less than " + value2);
 }else{
-  console.log(value1 + " is greather than or equal to " + value2);
+  console.log(value1 + " is greather than " + value2);
 }
